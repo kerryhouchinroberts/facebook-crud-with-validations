e20c8d6f509c6c258406f811366c1c4bdae02ea0
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-20 11:20:22 -0700

    Define index, show, new, and create in controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,15 +1,27 @@
 class StatusesController < ApplicationController
 
   def index
+    @statuses = Status.all
   end
 
   def show
+    @status = Status.find[params(:id)]
   end
 
   def new
+    @status = Status.new
   end
 
   def create
+    @status = Status.new(status_params)
+    if @status.save
+      redirect to status_show[params(:id)], notice: 'Status successfully created.'
+    else
+      flash.now[:alert] = 'There were some errors that prevented your status from being saved'
+      flash.now[:alert] << "<br>"
+      flash.now[:alert] << @status.errors.full_messages.join("<br>")
+      render :new
+    end
   end
 
   def edit
@@ -17,5 +29,9 @@ class StatusesController < ApplicationController
 
   def destroy
   end
+
+  private
+  def status_params
+    params.require(:status).permit(:user, :likes)
 
 end
