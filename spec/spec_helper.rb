ef84b246bbf285cdbe69f72d27d6822ae44d85e1
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-02-20 14:30:09 -0700

    Add rspec & capybara

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+
+RSpec.configure do |config|
+
+  require 'capybara/rspec'
+  require 'capybara/rails'
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
